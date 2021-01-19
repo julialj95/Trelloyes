53128a8006831654d132c554141bc29be14882de
Julia Johnson <julialj95@gmail.com>
2021-01-19 11:12:32 -0600

    button functionality added

--- src/card.js
+++ src/card.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./card.css";
 
-function Card(props) {
+export default function Card(props) {
   return (
     <div className="Card">
-      <button type="button">delete</button>
+      <button type="button" onClick={() => props.onClickDelete(props.id)}>
+        delete
+      </button>
       <h3>{props.title}</h3>
       <p>{props.content}</p>
     </div>
   );
 }
 
-export default Card;
+Card.propTypes = {
+  onClickDelete: () => {},
+};
--- src/list.js
+++ src/list.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Card from './card';
-import './list.css';
+import React from "react";
+import Card from "./card";
+import "./list.css";
 
 function List(props) {
   return (
@@ -9,18 +9,28 @@ function List(props) {
         <h2>{props.header}</h2>
       </header>
       <div className="List-cards">
-        {props.cards.map((card) =>
-        <Card
-          key={card.id}
-          title={card.title}
-          content={card.content}
-          /> 
-        )}
-        </div>
-      <button type="button" className="List-add-button">
-        + Add Random Card
-      </button>
+        {props.cards.map((card) => (
+          <Card
+            key={card.id}
+            id={card.id}
+            title={card.title}
+            content={card.content}
+            onClickDelete={props.onClickDelete}
+          />
+        ))}
+        <button
+          type="button"
+          className="List-add-button"
+          onClick={() => props.onClickAdd(props.id)}
+        >
+          + Add Random Card
+        </button>
+      </div>
     </section>
-  )
+  );
 }
+
+List.defaultProps = {
+  onClickAdd: () => {},
+};
 export default List;
